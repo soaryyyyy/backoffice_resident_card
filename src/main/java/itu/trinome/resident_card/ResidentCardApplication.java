5250e2ccbf6cef3a2c1e11f15892c3eca3528f28
Soary <rabezandrinasoary@gmail.com>
2026-04-16 14:07:56 +0300

    architecture

---
+++ src/main/java/itu/trinome/resident_card/ResidentCardApplication.java
@@ -0,0 +1,13 @@
+package itu.trinome.backoffice_resident_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResidentCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itu/trinome/resident_card/ResidentCardApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.trinome.backoffice_resident_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResidentCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
